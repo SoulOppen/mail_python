f1f6954aa5d810d962432a5c31c2cb87dc22f825
souloppen <arieloppenheimer@outlook.cl>
2025-07-13 23:03:54 -0400

    add:ingreso primer valor

---
+++ config.py
@@ -0,0 +1,7 @@
+Task=["Modificar excel",
+      "Obtener data estadística de excel",
+      "Enviar mail masivos con outlook",
+      "Enviar whatsApp"]
+Basic=[
+    "exit"
+]
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from config import Task,Basic 
 def main():
-    tasks=["Modificar excel","Obtener data estadística de excel","Enviar mail masivos con outlook","Enviar whatsApp"]
     print("Bienvenido a tu administrador de tareas repetitivas en windows")
     print("Las tareas que podemos administrar hasta el momento son:")
+    tasks=Task+Basic
     for i,task in enumerate(tasks):
         print(f"\t{i+1}-{task}")
+    try:
+        input_number=int(input("Numero de tarea: "))
+        print(tasks[input_number-1])
+    except ValueError:
+        print("Debes ingresar un número válido.")
+        main()
 if __name__=="__main__":
     main()
